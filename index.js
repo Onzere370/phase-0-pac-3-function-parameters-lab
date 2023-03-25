472a9c9ff45e2304edd6db6aa2f0d281a7b12dc6
cynthia <cynthiakagz@gmail.com>
2023-03-25 23:00:53 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,8 @@
+function introduction(name = Aki) {
+  console.log (name);
+return `Hi, my name is ${name}`; 
+}
+function introductionWithLanguage(name,language) {
+  console.log(` ${name} and ${language}.`);
+  return "Hi, my name is Aki and I am learning to program in Ember.js."
+}
